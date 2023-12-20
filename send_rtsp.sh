ab4e3a63262bfa798aea40667a787bddd21ec569
Guy Porat <guyp@hailo.ai>
2023-12-20 17:17:31 +0200

    update cmake

--- send_rtsp.sh
+++ send_rtsp.sh
@@ -5,4 +5,11 @@ t. ! queue ! udpsink host=127.0.0.1 port=5000 sync=false     \
 t. ! queue ! udpsink host=127.0.0.1 port=5001 sync=false     \
 t. ! queue ! udpsink host=127.0.0.1 port=5002 sync=false     \
 t. ! queue ! udpsink host=127.0.0.1 port=5002 sync=false     \
-t. ! queue ! udpsink host=127.0.0.1 port=5003 sync=false
+t. ! queue ! udpsink host=127.0.0.1 port=5003 sync=false     \
+t. ! queue ! udpsink host=127.0.0.1 port=5004 sync=false     \
+t. ! queue ! udpsink host=127.0.0.1 port=5005 sync=false     \
+t. ! queue ! udpsink host=127.0.0.1 port=5006 sync=false     \
+t. ! queue ! udpsink host=127.0.0.1 port=5007 sync=false     \
+t. ! queue ! udpsink host=127.0.0.1 port=5008 sync=false     \
+t. ! queue ! udpsink host=127.0.0.1 port=5009 sync=false     \
+t. ! queue ! udpsink host=127.0.0.1 port=5010 sync=false     
